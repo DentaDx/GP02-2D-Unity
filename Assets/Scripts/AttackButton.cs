631cceb4f76faab06ff5e4c684d85b6f4499f7fe
DentaDx <55885655+DentaDx@users.noreply.github.com>
2023-03-01 13:09:41 +0700

    Add files via upload

---
+++ Assets/Scripts/AttackButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackButton : MonoBehaviour
+{
+    private Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void AttackAnimation()
+    {
+        Anim.SetTrigger("isAttack");
+    }
+}
